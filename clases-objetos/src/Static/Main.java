79447b833ee447f05d7606d6685666295cf8d538
hhumbertogar <humbertogq87@gmail.com>
2025-04-27 17:50:01 -0700

    ft(static): static blocks are always triggered even before main method

--- clases-objetos/src/Static/Main.java
+++ clases-objetos/src/Static/Main.java
@@ -1,7 +1,15 @@
 package Static;
 
 public class Main {
+    static String appName;
+
+    static {
+        appName = "Mi app en Java";
+        System.out.println("appName = " + appName);
+    }
+
     public static void main(String[] args) {
+//        System.out.println("appName = " + appName);
         Counter counter1 = new Counter();
         Counter counter2 = new Counter();
         Counter counter3 = new Counter();
