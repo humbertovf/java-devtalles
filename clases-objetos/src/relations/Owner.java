bdc578b555673014e52ed23c5688d484cd1bb121
hhumbertogar <humbertogq87@gmail.com>
2025-04-27 18:28:24 -0700

    ft(class-relationships): dependency, composition and asociation

---
+++ clases-objetos/src/relations/Owner.java
@@ -0,0 +1,13 @@
+package relations;
+
+public class Owner {
+    private String name;
+
+    public Owner(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ clases-objetos/src/relations/Room.java
@@ -0,0 +1,13 @@
+package relations;
+
+public class Room {
+    private  String name;
+
+    public Room(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
